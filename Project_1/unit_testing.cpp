a65ef29d83389e740e0584e8bf50d0852084b0b7
Joker <lefte1998di@gmail.com>
2019-11-09 20:28:54 +0200

    added unit_testing

---
+++ Project_1/unit_testing.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <bitset>
+#include <climits>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <random>
+
+#include "./sortingAlg/quicksort.hpp"
+#include "./searchingAlg/binarySearch.hpp"
+
+using namespace std;
+
+int main(){
+    
+}
