bbff493df65f3a6ec879faf6549e5c75986b281d
LefterisDi <xan.michael98@yahoo.com>
2019-11-10 15:52:04 +0200

    Testing Files

--- Project_1/temp.cpp
+++ Project_1/temp.cpp
@@ -6,23 +6,27 @@ using namespace std;
 
 int main(int argc , char* argv[])
 {
-    List<int> list(20 , sizeof(int));
+    // List<int> list(20 , sizeof(int));
+    //
+    // list.ListInsert(1);
+    // list.ListInsert(2);
+    // list.ListInsert(3);
+    // list.ListInsert(4);
+    // list.ListInsert(5);
+    // list.ListInsert(6);
+    // list.ListInsert(7);
+    //
+    // list.ListPrint();
+    //
+    // uint64_t list_entry = 1;
+    // list_entry <<= 32;
+    // list_entry |= 2;
 
-    list.ListInsert(1);
-    list.ListInsert(2);
-    list.ListInsert(3);
-    list.ListInsert(4);
-    list.ListInsert(5);
-    list.ListInsert(6);
-    list.ListInsert(7);
+    // cout << list_entry << endl;
 
-    list.ListPrint();
-
-    uint64_t list_entry = 1;
-    list_entry <<= 32;
-    list_entry |= 2;
-
-    cout << list_entry << endl;
+    cout << sizeof(unsigned long) << endl;
+    cout << sizeof(unsigned long long) << endl;
+    cout << sizeof(uint64_t) << endl;
 
     // std::cout << bck.GetBucketSize() << '\n';
 }
