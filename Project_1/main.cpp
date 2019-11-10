c5e8dd5da1d34d9b6831ffc595a69d95a08ba712
LefterisDi <xan.michael98@yahoo.com>
2019-11-10 15:51:49 +0200

    Updated Main for Reading Dataset

--- Project_1/main.cpp
+++ Project_1/main.cpp
@@ -10,73 +10,105 @@
 
 #include "./sortingAlg/tablesort.hpp"
 #include "./templates/list.hpp"
+#include "./opts/getopts.hpp"
 #include "./utils/utils.hpp"
 
 using namespace std;
 
 int main(int argc , char* argv[])
 {
-    int size1x = 1000000 , size1y = 4;
-    int size2x = 10      , size2y = 2;
-    uint64_t** table1;
-    uint64_t** table2;
-    uint32_t* rowIDs1;
-    uint32_t* rowIDs2;
+    struct opt_types args[2];
+
+    /*
+     * +------------------------------------+
+     * | args[0].optType.cp = <relA file>   |
+     * | args[1].optType.cp = <relB file>   |
+     * | args[1].optType.cp = <query file>  |
+     * +------------------------------------+
+     */
+    if (!getopts(argc,argv,"ra:p,rb:p",args))
+        return -1;
+
+    cout << args[0].optType.cp << endl;
+    cout << args[1].optType.cp << endl;
+
+    // int tbl1_cols = 1000000 , tbl1_rows = 4;
+    // int tbl2_cols = 10      , tbl2_rows = 2;
+
+    uint32_t tbl1_cols, tbl1_rows;
+    uint32_t tbl2_cols, tbl2_rows;
+
+    uint64_t** table1 = ReadFile(args[0].optType.cp, tbl1_rows, tbl1_cols);
+    uint64_t** table2 = ReadFile(args[1].optType.cp, tbl2_rows, tbl2_cols);
+
+    // cout << "ROWS = " << tbl1_rows << endl;
+    // cout << "COLS = " << tbl1_cols << endl;
+    // for (int i = 0; i < tbl1_rows; i++) {
+    //     for (int j = 0; j < tbl1_cols; j++) {
+    //         cout << table1[i][j] << " , ";
+    //     }
+    //     cout << endl;
+    // }
+    // return 0;
+    // uint64_t** table2;
+
+    uint32_t*  rowIDs1;
+    uint32_t*  rowIDs2;
 
     // 1048576 Bytes = 1 MB
     List<uint64_t> list(1048576 , sizeof(uint64_t));
 
-    default_random_engine gen;
-    uniform_int_distribution<uint64_t> distribution(1,ULLONG_MAX);
+    // default_random_engine gen;
+    // uniform_int_distribution<uint64_t> distribution(1,ULLONG_MAX);
 
-    table1 = new uint64_t*[size1y];
-    for(int i = 0; i < size1y; i++)
-        table1[i] = new uint64_t[size1x];
+    // table1 = new uint64_t*[tbl1_rows];
+    // for(int i = 0; i < tbl1_rows; i++)
+    //     table1[i] = new uint64_t[tbl1_cols];
+    //
+    // table2 = new uint64_t*[tbl2_rows];
+    // for(int i = 0; i < tbl2_rows; i++)
+    //     table2[i] = new uint64_t[tbl2_cols];
 
-    table2 = new uint64_t*[size2y];
-    for(int i = 0; i < size2y; i++)
-        table2[i] = new uint64_t[size2x];
-
-    rowIDs1 = new uint32_t[size1x];
-    rowIDs2 = new uint32_t[size2x];
+    rowIDs1 = new uint32_t[tbl1_cols];
+    rowIDs2 = new uint32_t[tbl2_cols];
 
     // gen.seed ((unsigned int) time (NULL));
-    gen.seed(3);
+    // gen.seed(3);
 
-    for (int i = 0; i < size1x ; i++){
+    for (int i = 0; i < tbl1_cols ; i++){
         rowIDs1[i] = i;
     }
 
-    for (int i = 0; i < size2x ; i++){
+    for (int i = 0; i < tbl2_cols ; i++){
         rowIDs2[i] = i;
     }
 
-    for (int i = 0 ; i < size1y ; i++){
-        for (int j = 0 ; j < size1x ; j++){
-            table1[i][j] = distribution(gen);
-            // cout << table1[i][j] << " ";
-        }
-        // cout << endl;
-    }
-    // cout << endl;
-
-    for (int i = 0 ; i < size2y ; i++){
-        for (int j = 0 ; j < size2x ; j++){
-            table2[i][j] = distribution(gen);
-            // cout << table2[i][j] << " ";
-        }
-        // cout << endl;
-    }
-
-
-
-    TableSortOnKey(table1 , rowIDs1 , size1x , size1y , 0);
-    TableSortOnKey(table2 , rowIDs2 , size2x , size2y , 0);
+    // for (int i = 0 ; i < tbl1_rows ; i++){
+    //     for (int j = 0 ; j < tbl1_cols ; j++){
+    //         table1[i][j] = distribution(gen);
+    //         // cout << table1[i][j] << " ";
+    //     }
+    //     // cout << endl;
+    // }
+    // // cout << endl;
+    //
+    // for (int i = 0 ; i < tbl2_rows ; i++){
+    //     for (int j = 0 ; j < tbl2_cols ; j++){
+    //         table2[i][j] = distribution(gen);
+    //         // cout << table2[i][j] << " ";
+    //     }
+    //     // cout << endl;
+    // }
 
 
 
-    // for (int i = 0 ; i < size1y ; i++){
-    //     for (int j = 0 ; j < size1x ; j++){
+    TableSortOnKey(table1 , rowIDs1 , tbl1_cols , tbl1_rows , 0);
+    TableSortOnKey(table2 , rowIDs2 , tbl2_cols , tbl2_rows , 0);
+
+
+
+    // for (int i = 0 ; i < tbl1_rows ; i++){
+    //     for (int j = 0 ; j < tbl1_cols ; j++){
     //         cout << table1[i][j] << " ";
     //     }
     //     cout << endl;
@@ -84,8 +116,8 @@ int main(int argc , char* argv[])
 
     // cout << endl;
 
-    // for (int i = 0 ; i < size2y ; i++){
-    //     for (int j = 0 ; j < size2x ; j++){
+    // for (int i = 0 ; i < tbl2_rows ; i++){
+    //     for (int j = 0 ; j < tbl2_cols ; j++){
     //         cout << table2[i][j] << " ";
     //     }
     //     cout << endl;
@@ -93,25 +125,25 @@ int main(int argc , char* argv[])
 
     // cout << endl;
 
-    // for (int i = 0 ; i < size1x ; i++){
+    // for (int i = 0 ; i < tbl1_cols ; i++){
     //     cout << rowIDs1[i] << " ";
     // }
     // cout << endl;
 
-    // for (int i = 0 ; i < size2x ; i++){
+    // for (int i = 0 ; i < tbl2_cols ; i++){
     //     cout << rowIDs2[i] << " ";
     // }
     // cout << endl;
 
     // cout << "DONE" << endl;
 
-    MergeTables(list, table1 , rowIDs1 , size1x , 0 , table2 , rowIDs2 , size2x , 0);
+    MergeTables(list, table1 , rowIDs1 , tbl1_cols , 0 , table2 , rowIDs2 , tbl2_cols , 0);
 
-    for(int i = 0; i < size1y; ++i)
+    for(int i = 0; i < tbl1_rows; ++i)
         delete[] table1[i];
     delete[] table1;
 
-    for(int i = 0; i < size2y; ++i)
+    for(int i = 0; i < tbl2_rows; ++i)
         delete[] table2[i];
     delete[] table2;
 
