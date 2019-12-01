2312fefe5725e172175cf98d9d0c6645d9b9fe02
Joker <lefte1998di@gmail.com>
2019-12-01 20:01:00 +0200

    relation structures

--- relationStructs.h
+++ relationStructs.h
@@ -9,4 +9,19 @@ typedef struct RelationTable{
     uint64_t** table;
 }RelationTable;
 
+typedef struct JoinPred{
+    int type;
+    uint64_t* rel1;
+    uint64_t* rel2;
+    uint64_t  colRel1;
+    uint64_t  colRel2;
+}JoinPred;
+
+typedef struct CompPred{
+    int type;
+    uint64_t* rel1;
+    uint64_t  colRel1;
+    uint64_t  comp;
+}CompPred;
+
 #endif
