c3382d6d740e810a0eedc7ea42a36c42eeba3a82
Joker <lefte1998di@gmail.com>
2019-12-10 11:57:39 +0200

    commented main

--- main.cpp
+++ main.cpp
@@ -106,34 +106,34 @@ List<uint64_t>* ComparisonPredicate(RelationTable* relTable , CompPred& cpred , 
 
 int main(int argc , char* argv[])
 {
-    list<JoinPred> joinList;
-    list<JoinPred>::iterator itjp;
-    list<CompPred> compList;
-    list<CompPred>::iterator itcp;
+    // list<JoinPred> joinList;
+    // list<JoinPred>::iterator itjp;
+    // list<CompPred> compList;
+    // list<CompPred>::iterator itcp;
 
 
-    // uint32_t*  rowIDs1;
-    // uint32_t*  rowIDs2;
+    // // uint32_t*  rowIDs1;
+    // // uint32_t*  rowIDs2;
 
-    int relations = 3;
-    RelationTable* relTable = new RelationTable[relations];
-    relTable[0].rows = 4;
-    relTable[0].cols = 5;
-    relTable[0].table = new uint64_t*[relTable[0].cols];
-    for(int i = 0; i < relTable[0].cols; i++)
-        relTable[0].table[i] = new uint64_t[relTable[0].rows];
+    // int relations = 3;
+    // RelationTable* relTable = new RelationTable[relations];
+    // relTable[0].rows = 4;
+    // relTable[0].cols = 5;
+    // relTable[0].table = new uint64_t*[relTable[0].cols];
+    // for(int i = 0; i < relTable[0].cols; i++)
+    //     relTable[0].table[i] = new uint64_t[relTable[0].rows];
 
-    relTable[1].rows = 6;
-    relTable[1].cols = 3;
-    relTable[1].table = new uint64_t*[relTable[1].cols];
-    for(int i = 0; i < relTable[1].cols; i++)
-        relTable[1].table[i] = new uint64_t[relTable[1].rows];
+    // relTable[1].rows = 6;
+    // relTable[1].cols = 3;
+    // relTable[1].table = new uint64_t*[relTable[1].cols];
+    // for(int i = 0; i < relTable[1].cols; i++)
+    //     relTable[1].table[i] = new uint64_t[relTable[1].rows];
 
-    relTable[2].rows = 6;
-    relTable[2].cols = 4;
-    relTable[2].table = new uint64_t*[relTable[2].cols];
-    for(int i = 0; i < relTable[2].cols; i++)
-        relTable[2].table[i] = new uint64_t[relTable[2].rows];
+    // relTable[2].rows = 6;
+    // relTable[2].cols = 4;
+    // relTable[2].table = new uint64_t*[relTable[2].cols];
+    // for(int i = 0; i < relTable[2].cols; i++)
+    //     relTable[2].table[i] = new uint64_t[relTable[2].rows];
 
 
 
@@ -141,104 +141,110 @@ int main(int argc , char* argv[])
     uniform_int_distribution<uint64_t> distribution(1,5);
     // uniform_int_distribution<uint64_t> distribution(1,ULLONG_MAX);
 
-    // rowIDs1 = new uint32_t[size1x];
-    // rowIDs2 = new uint32_t[size2x];
+    // // rowIDs1 = new uint32_t[size1x];
+    // // rowIDs2 = new uint32_t[size2x];
 
     // gen.seed((unsigned int) time(NULL));
     gen.seed(2);
 
-    // for (int i = 0 ; i < size1x ; i++)
-    //     rowIDs1[i] = i;
+    // // for (int i = 0 ; i < size1x ; i++)
+    // //     rowIDs1[i] = i;
 
-    // for (int i = 0 ; i < size2x ; i++)
-    //     rowIDs2[i] = i;
-    for (int l = 0 ; l < relations ; l++){
-        for (int i = 0 ; i < relTable[l].cols ; i++) {
-            for (int j = 0 ; j < relTable[l].rows ; j++) {
-                relTable[l].table[i][j] = distribution(gen);
-            }
-        }
-    }
+    // // for (int i = 0 ; i < size2x ; i++)
+    // //     rowIDs2[i] = i;
+    // for (int l = 0 ; l < relations ; l++){
+    //     for (int i = 0 ; i < relTable[l].cols ; i++) {
+    //         for (int j = 0 ; j < relTable[l].rows ; j++) {
+    //             relTable[l].table[i][j] = distribution(gen);
+    //         }
+    //     }
+    // }
 
-    for (int l = 0 ; l < relations ; l++){
-        for (int i = 0 ; i < relTable[l].rows ; i++) {
-            for (int j = 0 ; j < relTable[l].cols ; j++) {
-                cout << relTable[l].table[j][i] << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
-
-    
-
-    JoinPred jp;
-    CompPred cp;
-    jp.type = 0;
-    jp.rel1 = 0;
-    jp.rel2 = 1;
-    jp.colRel1 = 1;
-    jp.colRel2 = 2;
-    joinList.push_back(jp);
-    jp.rel1 = 1;
-    jp.rel2 = 2;
-    jp.colRel1 = 0;
-    jp.colRel2 = 1;
-    joinList.push_back(jp);
-    cp.type = 1;
-    cp.rel1 = 0;
-    cp.colRel1 = 1;
-    cp.comp = '<';
-    cp.num = (uint64_t)3536385184864502858;
-    compList.push_back(cp);
+    // for (int l = 0 ; l < relations ; l++){
+    //     for (int i = 0 ; i < relTable[l].rows ; i++) {
+    //         for (int j = 0 ; j < relTable[l].cols ; j++) {
+    //             cout << relTable[l].table[j][i] << " ";
+    //         }
+    //         cout << endl;
+    //     }
+    //     cout << endl;
+    // }
 
     
-    List<uint64_t>* rowIDlist;
-    List<FullResList>* resList = new List<FullResList>(sizeof(ResStruct) , sizeof(ResStruct));
 
-    for (itcp = compList.begin(); itcp != compList.end(); itcp++){
-        //do every comp
-        rowIDlist = ComparisonPredicate(relTable , *itcp , *resList);
+    // JoinPred jp;
+    // CompPred cp;
+    // jp.type = 0;
+    // jp.rel1 = 0;
+    // jp.rel2 = 1;
+    // jp.colRel1 = 1;
+    // jp.colRel2 = 2;
+    // joinList.push_back(jp);
+    // jp.rel1 = 1;
+    // jp.rel2 = 2;
+    // jp.colRel1 = 0;
+    // jp.colRel2 = 1;
+    // joinList.push_back(jp);
+    // cp.type = 1;
+    // cp.rel1 = 0;
+    // cp.colRel1 = 1;
+    // cp.comp = '<';
+    // cp.num = (uint64_t)3536385184864502858;
+    // compList.push_back(cp);
+
+    
+    // List<uint64_t>* rowIDlist;
+    // List<FullResList>* resList = new List<FullResList>(sizeof(ResStruct) , sizeof(ResStruct));
+
+    // for (itcp = compList.begin(); itcp != compList.end(); itcp++){
+    //     //do every comp
+    //     rowIDlist = ComparisonPredicate(relTable , *itcp , *resList);
        
-        Bucket<uint64_t>* bucket = rowIDlist->GetFirst();
-
-        while (bucket != NULL) {
-            for (int j = 0 ; j < bucket->GetBucketItems() ; j++) {
-                cout << (*bucket)[j] << endl;
-            }
-
-            bucket = bucket->GetNextBucket();
-        }
-    }
-
-    // List<uint64_t>* rowIDlistJoin;
-    // list<uint64_t>::iterator itj;
-
-    // for (itjp = joinList.begin(); itjp != joinList.end() ; itjp++){
-    //     //do joins
-    //     rowIDlistJoin = JoinPredicate(relTable , *itjp);
-
-    //     Bucket<uint64_t>* bucket = rowIDlistJoin->GetFirst();
-
-    //     uint64_t mask32_left  = 0xFFFFFFFF;
-    //     uint32_t mask32_right = 0xFFFFFFFF;
-    //     mask32_left <<= 32;
+    //     Bucket<uint64_t>* bucket = rowIDlist->GetFirst();
 
     //     while (bucket != NULL) {
     //         for (int j = 0 ; j < bucket->GetBucketItems() ; j++) {
-    //             cout << (((*bucket)[j] & mask32_left) >> 32) << " , " << ((*bucket)[j] & mask32_right) << endl;
+    //             cout << (*bucket)[j] << endl;
     //         }
 
     //         bucket = bucket->GetNextBucket();
     //     }
     // }
 
+    // // List<uint64_t>* rowIDlistJoin;
+    // // list<uint64_t>::iterator itj;
+
+    // // for (itjp = joinList.begin(); itjp != joinList.end() ; itjp++){
+    // //     //do joins
+    // //     rowIDlistJoin = JoinPredicate(relTable , *itjp);
+
+    // //     Bucket<uint64_t>* bucket = rowIDlistJoin->GetFirst();
+
+    // //     uint64_t mask32_left  = 0xFFFFFFFF;
+    // //     uint32_t mask32_right = 0xFFFFFFFF;
+    // //     mask32_left <<= 32;
+
+    // //     while (bucket != NULL) {
+    // //         for (int j = 0 ; j < bucket->GetBucketItems() ; j++) {
+    // //             cout << (((*bucket)[j] & mask32_left) >> 32) << " , " << ((*bucket)[j] & mask32_right) << endl;
+    // //         }
+
+    // //         bucket = bucket->GetNextBucket();
+    // //     }
+    // // }
 
 
-    for (int l = 0 ; l < relations ; l++){
-        for(int i = 0; i < relTable[l].cols; i++)
-            delete[] relTable[l].table[i];
-        delete[] relTable[l].table;
+
+    // for (int l = 0 ; l < relations ; l++){
+    //     for(int i = 0; i < relTable[l].cols; i++)
+    //         delete[] relTable[l].table[i];
+    //     delete[] relTable[l].table;
+    // }
+
+    List<uint64_t>l(10*sizeof(uint64_t) , sizeof(uint64_t));
+
+    for (int i = 0 ; i < 100 ; i++){
+        l.ListInsert(distribution(gen));
     }
     
 }
