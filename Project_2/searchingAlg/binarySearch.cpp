1d7f68eb51501ba9c0875d6b7f6aa9a0fc0e3eb3
LefterisDi <xan.michael98@yahoo.com>
2019-12-18 23:30:13 +0200

    commit

---
+++ Project_2/searchingAlg/binarySearch.cpp
@@ -0,0 +1,37 @@
+/* File: binarySearch.cpp */
+
+#include <iostream>
+#include <cstdlib>
+
+int binarySearch(uint64_t arr[], bool* positionCTable, int l, int r, uint64_t x)
+{
+    if (r >= l)
+    {
+        int mid = l + (r - l) / 2;
+
+        // If the element is present at the middle itself
+        if (arr[mid] == x && positionCTable[mid] == false)//checks if the element's position has been used before
+            return mid;
+
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            return binarySearch(arr , positionCTable , l, mid - 1, x);
+
+        else if (arr[mid] < x)
+            return binarySearch(arr , positionCTable , mid + 1, r, x);
+
+        else if (arr[mid] == x && positionCTable[mid] == true) {
+            int ind = binarySearch(arr , positionCTable, l, mid - 1, x);
+
+            if (ind == -1)
+                return binarySearch(arr , positionCTable , mid + 1, r, x);
+            else
+                return ind;
+        }
+        // Else the element can only be present in right subarray
+    }
+
+    // We reach here when element is not present in array
+    return -1;
+}
---
+++ Project_2/utils/predicates.hpp
@@ -0,0 +1,20 @@
+/* File: predicates.cpp */
+
+#ifndef __PREDICATES_HEADER__
+#define __PREDICATES_HEADER__
+
+#include "../utils/relationStructs.hpp"
+
+ResStruct* FindInResList      (List<ResStruct> *, uint64_t);
+
+int        DeleteTargeted     (FullResList   *, int, List<uint64_t> *);
+int        DeleteTargetedSL   (ResStruct     *, int, List<uint64_t> *);
+
+int        JoinSelf           (RelationTable **, JoinPred &, List<FullResList> *);
+int        JoinPredicate      (RelationTable **, JoinPred &, List<FullResList> *);
+int        ComparisonPredicate(RelationTable **, CompPred &, List<FullResList> *);
+
+int        DoAllCompPreds     (RelationTable **, List<CompPred> *, List<FullResList> *, bool *);
+int        DoAllJoinPreds     (RelationTable **, List<JoinPred> *, List<FullResList> *, bool *);
+
+#endif // __PREDICATES_HEADER__
