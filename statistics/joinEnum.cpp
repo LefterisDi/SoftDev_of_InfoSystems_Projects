fcac43f5c7342ee0db145feccb3529d1bac32388
LefterisDi <lefte1998di@gmail.com>
2020-01-21 17:11:34 +0200

    changes

--- statistics/joinEnum.cpp
+++ statistics/joinEnum.cpp
@@ -13,6 +13,8 @@
 #include "../Jobs/Jobs.hpp"
 #include "../JobScheduler/JobScheduler.hpp"
 
+using namespace std;
+
 
 // A function to print all combination of a given length from the given array.
 void GenSubSet(MiniVector<MiniVector<uint32_t>* >& resVec , int reqLen, int start, int currLen, bool check[], uint16_t len) 
@@ -82,26 +84,30 @@ bool connected( MiniVector<uint32_t>* set , int relNum , List<JoinPred>* joinLis
 
 int findPlace(MiniVector<uint32_t>* set , int relNum = -1){
 
+	if (set == NULL){
+		perror("Set vector is NULL");
+		exit(1);
+	}
+
 	int res = 0;
-	uint32_t pl = set->GetTotalItems();
-	for (uint32_t i = 0 ; i < pl ; i++){
-		uint32_t st = (*set)[i];
-		uint32_t slide = (1<<st);
-		res = res + slide;
+	for (uint32_t i = 0 ; i < set->GetTotalItems() ; i++){
+		uint32_t slide = 1;
+		res = res + slide<<(*set)[i];
 	}
 
 	if (relNum > -1){
-		res = res + (1<<relNum);
+		uint32_t slide = 1;
+		res = res + (slide<<relNum);
 	}
 
 	return res;
 }
 
 
-bool ExistsInJheRels(JoinHashEntry& jhe , int relNum){
+bool ExistsInJheRels(JoinHashEntry* jhe , int relNum){
 
-	for (int i = 0 ; i < jhe.rels.GetTotalItems() ; i++){
-		if (jhe.rels[i] == relNum){
+	for (uint32_t i = 0 ; i < jhe->rels.GetTotalItems() ; i++){
+		if (jhe->rels[i] == relNum){
 			return true;
 		}
 	}
@@ -109,10 +115,10 @@ bool ExistsInJheRels(JoinHashEntry& jhe , int relNum){
 	return false;
 }
 
-bool ExistsInJheJoinPreds(JoinHashEntry& jhe , int predNum){
+bool ExistsInJheJoinPreds(JoinHashEntry* jhe , int predNum){
 
-	for (int i = 0 ; i < jhe.vectJPnum.GetTotalItems() ; i++){
-		if (jhe.vectJPnum[i] == predNum){
+	for (int i = 0 ; i < jhe->vectJPnum.GetTotalItems() ; i++){
+		if (jhe->vectJPnum[i] == predNum){
 			return true;
 		}
 	}
@@ -137,70 +143,74 @@ uint64_t TreeCost(TableStats* relTableStats , JoinPred& jp){
 		return cost;
 }
 
-JoinHashEntry* CreateJoinTree(RelationTable** relTable , JoinHashEntry jhe , int relNum ,  List<JoinPred>* joinList){
+JoinHashEntry* CreateJoinTree(RelationTable** relTable , JoinHashEntry* jhe , int relNum ,  List<JoinPred>* joinList){
 
 	JoinHashEntry* newJhe = new JoinHashEntry;
 
-	*newJhe = jhe;
 	int indexKeeper = -1;
 	uint64_t minCost = INT_MAX;
+	
 
-	for (int i = 0 ; i < joinList->GetTotalItems() ; i++){
-		JoinPred* jp = &( (*(*joinList)[i])[0] );
+	// for (int i = 0 ; i < joinList->GetTotalItems() ; i++){
+	// 	JoinPred* jp = &( (*(*joinList)[i])[0] );
 		
-		if (jp->rel1 == relNum && ExistsInJheRels(jhe , jp->rel2) == true && ExistsInJheJoinPreds(jhe , i) == false ){
-			uint64_t newCost;
-			if ( ( newCost = TreeCost(jhe.relTableStats , *jp) ) < minCost){
-				minCost = newCost;
-				indexKeeper = i;
-			}
-		}
-		else if (jp->rel2 == relNum && ExistsInJheRels(jhe , jp->rel1) == true && ExistsInJheJoinPreds(jhe , i) == false ){
-			uint64_t newCost;
-			if ( ( newCost = TreeCost(jhe.relTableStats , *jp) ) < minCost){
-				minCost = newCost;
-				indexKeeper = i;
-			}
-		}
-	}
+	// 	if (jp->rel1 == relNum && ExistsInJheRels(jhe , jp->rel2) == true && ExistsInJheJoinPreds(jhe , i) == false ){
+	// 		uint64_t newCost;
+	// 		if ( ( newCost = TreeCost(jhe->relTableStats , *jp) ) < minCost){
+	// 			minCost = newCost;
+	// 			indexKeeper = i;
+	// 		}
+	// 	}
+	// 	else if (jp->rel2 == relNum && ExistsInJheRels(jhe , jp->rel1) == true && ExistsInJheJoinPreds(jhe , i) == false ){
+	// 		uint64_t newCost;
+	// 		if ( ( newCost = TreeCost(jhe->relTableStats , *jp) ) < minCost){
+	// 			minCost = newCost;
+	// 			indexKeeper = i;
+	// 		}
+	// 	}
+	// }
 
-	if (indexKeeper == -1){
-		perror("Index keeper -1 error!");
-		exit(1);
-	}
+	// if (indexKeeper == -1){
+	// 	perror("Index keeper -1 error!");
+	// 	exit(1);
+	// }
 
-	newJhe->cost = minCost;
-	newJhe->rels.PushBack(relNum);
-	newJhe->vectJPnum.PushBack(indexKeeper);
+	// newJhe->cost = minCost;
+	// for (int i = 0 ; i < jhe->rels.GetTotalItems() ; i++){
+	// 	newJhe->rels.
+	// }
+	// newJhe->rels.PushBack(relNum);
+	// newJhe->vectJPnum.PushBack(indexKeeper);
 
 	return newJhe;
 }
 
 JoinHashEntry* JoinEnumeration(RelationTable** relTable , uint16_t relTSize , List<JoinPred>* joinList)
 {
-    MyHashMap< int , JoinHashEntry > hmap( raiseToPower(2 , relTSize) - 1);
+    MyHashMap< int , JoinHashEntry* > hmap( raiseToPower(2 , relTSize) - 1);
 	JoinHashEntry* res = new JoinHashEntry;
 
 	int num = 1;
     for (int i = 0 ; i < relTSize ; i++){
-        JoinHashEntry jhe;
+       
+	    JoinHashEntry* jhe = new JoinHashEntry;
 		
-		jhe.vectJPnum.PushBack(-1);
-		jhe.rels.PushBack(i);
-		jhe.tableNum = relTSize;
-		jhe.relTableStats = new TableStats[relTSize];
+		// jhe.vectJPnum.PushBack(-1);
+		jhe->rels.PushBack(i);
+		jhe->tableNum = relTSize;
+		jhe->relTableStats = new TableStats[relTSize];
 
-		for (int i = 0; i < jhe.tableNum ; i++) {
+		for (int i = 0; i < jhe->tableNum ; i++) {
 			
-			jhe.relTableStats[i].cols = relTable[i]->cols;
-			jhe.relTableStats[i].statsPerCol = new Stats[relTable[i]->cols];
+			jhe->relTableStats[i].cols = relTable[i]->cols;
+			jhe->relTableStats[i].statsPerCol = new Stats[relTable[i]->cols];
 
-			for (int j = 0 ; j < jhe.relTableStats[i].cols ; j++){
-				jhe.relTableStats[i].statsPerCol[j] = relTable[i]->colStats[j];
+			for (int j = 0 ; j < jhe->relTableStats[i].cols ; j++){
+				jhe->relTableStats[i].statsPerCol[j] = relTable[i]->colStats[j];
 			}
 		}
 
-		jhe.cost = 0;
+		jhe->cost = 0;
         hmap.set( num , jhe);
 		num<<=1;
     }
@@ -213,11 +223,18 @@ JoinHashEntry* JoinEnumeration(RelationTable** relTable , uint16_t relTSize , Li
 		
         for (int sCount = 0 ; sCount < sets.GetTotalItems() ; sCount++) {
 
+			cout << "SIZE " << sets[sCount]->GetTotalItems() << endl;
+
+			for (uint32_t l = 0 ; l < sets[sCount]->GetTotalItems() ; l++){
+				cout << (*sets[sCount])[l] << " "; 
+			}
+			cout << endl;
+
 			for (int j = 0 ; j < relTSize ; j++) {
 	
 				bool isNull = false;
 				JoinHashEntry* jhe = NULL;
-				JoinHashEntry existingJHE;
+				JoinHashEntry* existingJHE = NULL;
 
                 if ( existsInS(sets[sCount] , j))
 					continue;
@@ -234,29 +251,47 @@ JoinHashEntry* JoinEnumeration(RelationTable** relTable , uint16_t relTSize , Li
 
 				jhe = CreateJoinTree( relTable , hmap.get( findPlace(sets[sCount]) ) , j , joinList );
 
-				try {
-					existingJHE =  hmap.get( findPlace(sets[sCount] , j) );
-				} catch(const std::invalid_argument& arg) {
-					isNull = true;
-				}
+	// 			try {
+	// 				existingJHE =  hmap.get( findPlace(sets[sCount] , j) );
+	// 			} catch(const std::invalid_argument& arg) {
+	// 				isNull = true;
+	// 			}
 
-				if (isNull == true) {
-					hmap.set(findPlace(sets[sCount] , j) , *jhe);
+	// 			if (isNull == true) {
+	// 				hmap.set(findPlace(sets[sCount] , j) , *jhe);
 				
-                } else if (existingJHE.cost > jhe->cost ) {
-					hmap.delete_key( findPlace(sets[sCount] , j) );
-					hmap.set(findPlace(sets[sCount] , j) , *jhe);
-				}
+    //             } else if (existingJHE.cost > jhe->cost ) {
+	// 				hmap.delete_key( findPlace(sets[sCount] , j) );
+	// 				hmap.set(findPlace(sets[sCount] , j) , *jhe);
+	// 			}
 			}
 			delete sets[sCount];
 		}
+
 	}
 
-	try {
-		*res = hmap.get( raiseToPower(2 , relTSize) - 1 ) ;
-	} catch(const std::invalid_argument& arg) {
-		perror("Error finding best path!");
-		exit(1);
+	// try {
+	// 	*res = hmap.get( raiseToPower(2 , relTSize) - 1 ) ;
+	// } catch(const std::invalid_argument& arg) {
+	// 	perror("Error finding best path!");
+	// 	exit(1);
+	// }
+
+	for (int i = 0; i < raiseToPower(2 , relTSize) - 1 ; i++){
+		JoinHashEntry* tmp = NULL;
+		try {
+			tmp = hmap.get(i);
+		} catch(const std::invalid_argument& arg) {
+			continue;
+		}
+
+		hmap.delete_key(i);
+
+		// for (int j = 0; j < tmp->tableNum ; j++) {
+		// 	delete tmp->relTableStats[i].statsPerCol;
+		// }
+
+		// delete tmp->relTableStats;
 	}
     
    return res;
